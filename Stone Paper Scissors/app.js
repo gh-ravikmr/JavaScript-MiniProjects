5812865baba221cc3ef1aa0a4cdc350f76f56db2
Ravi Kumar <ytravi1020120@gmail.com>
2025-02-16 19:39:06 +0530

    Decisions added and finalization added

--- Stone Paper Scissors/app.js
+++ Stone Paper Scissors/app.js
@@ -2,8 +2,54 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
-choices.forEach((choice)) => {
-    choice.addEventListener("click", () =>{
+const msg = document.querySelector("#msg")
 
-    })
-}
+const gencompchoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
+
+const drawGame = () => {
+  console.log("game was graw");
+  msg.innerText = "Game is Draw Play Again!"
+  msg.style.backgroundColor = "#081b31 "
+};
+
+const showwinner = (userWin) => {
+  if (userWin) {
+    console.log("you win");
+    msg.innerText = "You Win"
+    msg.style.backgroundColor = "green"
+  } else {
+    console.log("You Lost");
+    msg.innerText = "You lose."
+    msg.style.backgroundColor = "red"
+  }
+};
+
+const playGame = (userChoice) => {
+  const compChoice = gencompchoice();
+  console.log("com choice", compChoice);
+
+  if (userChoice === compChoice) {
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showwinner(userWin);
+  }
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
